0e5ac9367c17dae0e7f22878a83afbdc592e3587
santanu maity <santanumaityisback@gmail.com>
2022-10-03 21:41:01 +0530

    Test Case1
    I have done with  test case1.

--- santanu/src/main/java/com/qa/testbase/TestBase.java
+++ santanu/src/main/java/com/qa/testbase/TestBase.java
@@ -1,5 +1,24 @@
 package com.qa.testbase;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBase {
+	@Test
+	public void setUp()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://live.techpanda.org/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+	}
 
 }
